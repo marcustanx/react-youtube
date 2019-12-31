47618305b153945db56a6f7bb409abbbf2d875f1
Marcus Tan <marcus@mtss.dev>
2019-12-31 15:30:17 +0800

    Updates

---
+++ precache-manifest.c0709857495d6d8b4ebe4de577aad096.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "81b2fbdc516dd8a442f5",
+    "url": "/react-youtube/static/css/main.47c46259.chunk.css"
+  },
+  {
+    "revision": "81b2fbdc516dd8a442f5",
+    "url": "/react-youtube/static/js/main.81b2fbdc.chunk.js"
+  },
+  {
+    "revision": "102c3bf24ee7bc42981e",
+    "url": "/react-youtube/static/js/1.102c3bf2.chunk.js"
+  },
+  {
+    "revision": "4e3ce4fab19648a0b11d",
+    "url": "/react-youtube/static/js/runtime~main.4e3ce4fa.js"
+  },
+  {
+    "revision": "003e7a1e8aef16ee7fbe303c46a5cbca",
+    "url": "/react-youtube/index.html"
+  }
+];
