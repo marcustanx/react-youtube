977b48d05ae14acc9185fa25a8750905c03944f2
Marcus Tan <marcus@mtss.dev>
2019-12-31 17:57:10 +0800

    Update firebase hosting configs

---
+++ firebasesdk.js
@@ -0,0 +1,10 @@
+
+firebase.initializeApp({
+  "projectId": "website-preview2020",
+  "appId": "1:1019788298839:web:473843e5616a39ba5eb112",
+  "databaseURL": "https://website-preview2020.firebaseio.com",
+  "storageBucket": "website-preview2020.appspot.com",
+  "apiKey": "AIzaSyDuv2gSCME9TOBQx9P56wkP2KCZqZOMSJs",
+  "authDomain": "website-preview2020.firebaseapp.com",
+  "messagingSenderId": "1019788298839"
+});
